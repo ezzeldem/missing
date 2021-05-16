f4cc3e9c23f5649f9537a467122aed204a8603d5
ezzeldem <ezzelden06@gmail.com>
2021-05-16 11:52:55 +0200

    e

---
+++ assets/js/min_script/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  // web lang
+  if ($("html").attr("dir") == "rtl") {
+    var dir_lang = true;
+  } else {
+    var dir_lang = false;
+  }
+
+
+
+
+     // missing_slider
+  $(".missing_slider").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: true,
+    autoplay:true,
+    rtl: dir_lang,
+    dots: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 3000,
+    prevArrow: "<div class='circle-arrow arrows_left'><i class='fa fa-chevron-left'></i></div>",
+    nextArrow: "<div class='circle-arrow arrows_right'><i class='fa fa-chevron-right'></i></div>",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 772,
+        settings: {
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+
+
+
+     // counter plugin
+     $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+
+    // AOS
+    AOS.init({ duration: 600 });
+
+});
